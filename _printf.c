ae566a6eb5451a0573e79bbab552279cfdeaabee
kidanemariam <kide.fikadu1@gmail.com>
2022-07-11 15:06:28 +0000

    printf one

---
+++ _printf.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ * Return: number of chars printed.
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, len = 0;
+	va_list arguments;
+	char *buffer;
+
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if(!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{
+				return (-1);
+			}
+			else
+			{
+				len++;
+			}i++;
+		}else
+			len++;
+	}
+	return (len);
+}
