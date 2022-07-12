f0e22fad11de302a9239656b69bf0f5d8854bdb7
kidanemariam <kide.fikadu1@gmail.com>
2022-07-12 02:19:45 +0000

    restored to working code

--- _printf.c
+++ _printf.c
@@ -7,9 +7,8 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
+	unsigned int i = 0, len = 0;
 	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
 	char *buffer;
 
 	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
